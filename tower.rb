aec9762d32e66d94334f09afc5ee8323d9c1fbe2
Paul Mucur <mudge@mudge.name>
2016-06-28 19:48:02 +0100

    Pass pegs to tower

--- tower.rb
+++ tower.rb
@@ -1,15 +1,8 @@
-def tower(n)
-  # repeat
-  # until all disks are on one peg
-
-  a = n.downto(1).entries
-  b = []
-  c = []
-  pegs = [a, b, c]
+def tower(pegs)
+  a, b, c = pegs
+  n = a.length
 
   loop do
-
-    #   move smallest disk clockwise
     peg_with_smallest_disk = pegs.min_by { |peg| peg.last || Float::INFINITY }
     next_peg = pegs[pegs.index(peg_with_smallest_disk).succ % pegs.length]
     next_peg.push(peg_with_smallest_disk.pop)
@@ -17,7 +10,6 @@ def tower(n)
 
     break if b.length == n || c.length == n
 
-    #   move second disk to remaining peg
     peg_with_second_disk = (pegs - [next_peg]).min_by { |peg| peg.last || Float::INFINITY }
     remaining_peg = (pegs - [next_peg, peg_with_second_disk]).first
     remaining_peg.push(peg_with_second_disk.pop)
@@ -26,4 +18,5 @@ def tower(n)
   end
 end
 
-tower(4)
+pegs = (a = [5, 4, 3, 2, 1], b = [], c = [])
+tower(pegs)
