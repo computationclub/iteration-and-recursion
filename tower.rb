1aac44b33ee10e941e9fcc08f24a7048f7af163e
Paul Mucur <mudge@mudge.name>
2016-06-28 19:54:21 +0100

    Recursive version

--- tower.rb
+++ tower.rb
@@ -1,21 +1,18 @@
 def tower(pegs)
-  a, b, c = pegs
-  n = a.length
+  peg_with_smallest_disk = pegs.min_by { |peg| peg.last || Float::INFINITY }
+  next_peg = pegs[pegs.index(peg_with_smallest_disk).succ % pegs.length]
+  next_peg.push(peg_with_smallest_disk.pop)
+  p pegs
 
-  loop do
-    peg_with_smallest_disk = pegs.min_by { |peg| peg.last || Float::INFINITY }
-    next_peg = pegs[pegs.index(peg_with_smallest_disk).succ % pegs.length]
-    next_peg.push(peg_with_smallest_disk.pop)
-    p a: a, b: b, c: c
+  return if pegs.count { |peg| peg.empty? } == pegs.length.pred
 
-    break if b.length == n || c.length == n
+  peg_with_second_disk = (pegs - [next_peg]).min_by { |peg| peg.last || Float::INFINITY }
+  remaining_peg = (pegs - [next_peg, peg_with_second_disk]).first
+  remaining_peg.push(peg_with_second_disk.pop)
 
-    peg_with_second_disk = (pegs - [next_peg]).min_by { |peg| peg.last || Float::INFINITY }
-    remaining_peg = (pegs - [next_peg, peg_with_second_disk]).first
-    remaining_peg.push(peg_with_second_disk.pop)
+  p pegs
 
-    p a: a, b: b, c: c
-  end
+  tower(pegs)
 end
 
 pegs = (a = [5, 4, 3, 2, 1], b = [], c = [])
